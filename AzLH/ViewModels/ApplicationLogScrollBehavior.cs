9a5614ba58e8bcf47c0a647d9feb205acf2115a8
YSRKEN <ysr.ken@gmail.com>
2017-11-09 01:29:28 +0900

    ログ表示用のテキストボックスに内容が追加されると、自動でスクロールするようにした

---
+++ AzLH/ViewModels/ApplicationLogScrollBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace AzLH.ViewModels {
+	class ApplicationLogScrollBehavior : Behavior<TextBox> {
+		protected override void OnAttached() {
+			base.OnAttached();
+			this.AssociatedObject.TextChanged += TextBox_Changed;
+		}
+		protected override void OnDetaching() {
+			base.OnDetaching();
+			this.AssociatedObject.TextChanged -= TextBox_Changed;
+		}
+		void TextBox_Changed(object sender, EventArgs e) {
+			this.AssociatedObject.ScrollToEnd();
+		}
+	}
+}
--- AzLH/Views/MainView.xaml.cs
+++ AzLH/Views/MainView.xaml.cs
@@ -8,5 +8,9 @@ namespace AzLH.Views {
 		public MainView() {
 			InitializeComponent();
 		}
+
+		private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e) {
+
+		}
 	}
 }
