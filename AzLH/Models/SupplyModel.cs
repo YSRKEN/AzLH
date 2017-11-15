ac38418833413cffda8cf2d633e11be562f0c287
YSRKEN <ysr.ken@gmail.com>
2017-11-16 00:22:24 +0900

    ・SupplyView上にあるオブジェクトに対してイベントを貼り付けた ・ついでに表示する期間をBindingした

--- AzLH/Models/SupplyModel.cs
+++ AzLH/Models/SupplyModel.cs
@@ -1,4 +1,7 @@
 ﻿using Prism.Mvvm;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Linq;
 
 namespace AzLH.Models {
 	class SupplyModel : BindableBase {
@@ -10,5 +13,43 @@ namespace AzLH.Models {
 		// ダイヤ：～1000　特殊資材右
 		// 家具コイン：～1000　特殊資材右
 
+		// 表示する期間
+		private int graphPeriodIndex = 2;
+		public int GraphPeriodIndex {
+			get => graphPeriodIndex;
+			set { SetProperty(ref graphPeriodIndex, value); }
+		}
+		// 表示する期間の一覧(Keyが名称、Valueが期間(単位は"日"))
+		private Dictionary<string, double> graphPeriodDic = new Dictionary<string, double> {
+				{ "6時間", 0.25 },
+				{ "12時間", 0.5 },
+				{ "1日", 1 },
+				{ "3日", 3 },
+				{ "1週間", 7 },
+				{ "1ヶ月", 30 },
+				{ "3ヶ月", 90 },
+				{ "6ヶ月", 180 },
+				{ "1年", 365 },
+			};
+		public List<string> GraphPeriodList => graphPeriodDic.Keys.Select(p => p).ToList();
+		// 表示する資材のモード
+		private int showSupplyMode = 0;
+		// 表示する資材のモードに対応してボタンの色・表示内容が変わる
+		public Brush SupplyModeButtonColor => (showSupplyMode == 0 ? new SolidColorBrush(Colors.Pink) : new SolidColorBrush(Colors.SkyBlue));
+		public string SupplyModeButtonContent => (showSupplyMode == 0 ? "通常資材" : "特殊資材");
+
+		// 表示する資材のモードを切り替える
+		public void ChangeSupplyMode() {
+			if (showSupplyMode == 0)
+				showSupplyMode = 1;
+			else
+				showSupplyMode = 0;
+			RaisePropertyChanged("SupplyModeButtonColor");
+			RaisePropertyChanged("SupplyModeButtonContent");
+		}
+		// グラフ画像を保存する
+		public void SaveSupplyGraph() {
+			// スタブ
+		}
 	}
 }
--- AzLH/ViewModels/SupplyViewModel.cs
+++ AzLH/ViewModels/SupplyViewModel.cs
@@ -1,13 +1,41 @@
 ﻿using AzLH.Models;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System.Collections.Generic;
+using System.Windows.Media;
 
 namespace AzLH.ViewModels {
 	internal class SupplyViewModel {
 		// modelのinstance
 		private readonly SupplyModel supplyModel;
+		// 表示する期間
+		public ReactiveProperty<int> GraphPeriodIndex { get; }
+		// 表示する期間の一覧
+		public ReactiveProperty<List<string>> GraphPeriodList { get; }
+		// 表示する資材のモードに対応してボタンの色・表示内容が変わる
+		public ReactiveProperty<Brush> SupplyModeButtonColor { get; }
+		public ReactiveProperty<string> SupplyModeButtonContent { get; }
+
+		// 表示する資材のモードを切り替える
+		public ReactiveCommand ChangeSupplyModeCommand { get; }
+		// グラフ画像を保存する
+		public ReactiveCommand SaveSupplyGraphCommand { get; }
+
 		// コンストラクタ
 		public SupplyViewModel() {
 			// 初期化
 			supplyModel = new SupplyModel();
+			// プロパティを設定
+			GraphPeriodIndex = supplyModel.ToReactivePropertyAsSynchronized(x => x.GraphPeriodIndex);
+			GraphPeriodList = supplyModel.ObserveProperty(x => x.GraphPeriodList).ToReactiveProperty();
+			SupplyModeButtonColor = supplyModel.ObserveProperty(x => x.SupplyModeButtonColor).ToReactiveProperty();
+			SupplyModeButtonContent = supplyModel.ObserveProperty(x => x.SupplyModeButtonContent).ToReactiveProperty();
+			// コマンドを設定
+			ChangeSupplyModeCommand = new ReactiveCommand();
+			SaveSupplyGraphCommand = new ReactiveCommand();
+			//
+			ChangeSupplyModeCommand.Subscribe(supplyModel.ChangeSupplyMode);
+			SaveSupplyGraphCommand.Subscribe(supplyModel.SaveSupplyGraph);
 		}
 	}
 }
