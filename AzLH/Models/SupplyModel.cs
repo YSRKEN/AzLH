425a439e64c3876620d0d93e501982e872c2eeab
YSRKEN <ysr.ken@gmail.com>
2017-11-17 00:43:12 +0900

    グラフ線を描画した

--- AzLH/Models/SupplyModel.cs
+++ AzLH/Models/SupplyModel.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using OxyPlot;
 using System;
 using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace AzLH.Models {
 	class SupplyModel : BindableBase {
@@ -208,6 +209,7 @@ namespace AzLH.Models {
 			// スタブ
 		}
 		// グラフを再描画する
+		// 参考→https://qiita.com/maruh/items/035a39a2a01102ce248f
 		public void RedrawSupplyGraph() {
 			// Y軸および第二Y軸に表示するデータを集めておく
 			// Y軸→「表示する資材のモードと同じ」かつ「第二Y軸ではない」資材名
@@ -229,9 +231,21 @@ namespace AzLH.Models {
 			int maxYValue = AxisYList.Max(p => p.Value.Where(q => minDateTime <= q.Key).Max(r => r.Value));
 			int intervalY;
 			SpecialCeiling(ref maxYValue, out intervalY);
+			string axisYStr = "";
+			for (int i = 0; i < AxisYList.Count; ++i) {
+				if (i != 0)
+					axisYStr += "・";
+				axisYStr += AxisYList.Keys.ToList()[i];
+			}
 			int maxY2Value = AxisY2List.Max(p => p.Value.Where(q => minDateTime <= q.Key).Max(r => r.Value));
 			int intervalY2;
 			SpecialCeiling(ref maxY2Value, out intervalY2);
+			string axis2YStr = "";
+			for (int i = 0; i < AxisY2List.Count; ++i) {
+				if (i != 0)
+					axisYStr += "・";
+				axis2YStr += AxisY2List.Keys.ToList()[i];
+			}
 			// グラフ要素を構築する
 			//軸
 			var newSupplyGraphModel = new PlotModel();
@@ -243,7 +257,8 @@ namespace AzLH.Models {
 				MajorGridlineStyle = LineStyle.Solid,
 				MajorGridlineColor = OxyColors.LightGray,
 				MajorStep = NowGraphPeriodInfo.MajorStep,
-				MinorStep = NowGraphPeriodInfo.MinorStep
+				MinorStep = NowGraphPeriodInfo.MinorStep,
+				Title = "日時"
 			});
 			newSupplyGraphModel.Axes.Add(new LinearAxis {
 				Position = AxisPosition.Left,
@@ -252,7 +267,9 @@ namespace AzLH.Models {
 				MajorGridlineStyle = LineStyle.Solid,
 				MajorGridlineColor = OxyColors.LightGray,
 				MajorStep = intervalY,
-				MinorTickSize = 0
+				MinorTickSize = 0,
+				Title = axisYStr,
+				Key = "Primary"
 			});
 			newSupplyGraphModel.Axes.Add(new LinearAxis {
 				Position = AxisPosition.Right,
@@ -261,10 +278,35 @@ namespace AzLH.Models {
 				MajorGridlineStyle = LineStyle.Solid,
 				MajorGridlineColor = OxyColors.LightGray,
 				MajorStep = intervalY2,
-				MinorTickSize = 0
+				MinorTickSize = 0,
+				Title = axis2YStr,
+				Key = "Secondary"
 			});
 			//グラフ値
-
+			foreach(var plotInfo in AxisYList) {
+				var lineSeries = new LineSeries();
+				foreach(var plotData in plotInfo.Value) {
+					lineSeries.Points.Add(new DataPoint(
+						DateTimeAxis.ToDouble(plotData.Key),
+						plotData.Value
+					));
+				}
+				lineSeries.YAxisKey = "Primary";
+				lineSeries.Title = plotInfo.Key;
+				newSupplyGraphModel.Series.Add(lineSeries);
+			}
+			foreach (var plotInfo in AxisY2List) {
+				var lineSeries = new LineSeries();
+				foreach (var plotData in plotInfo.Value) {
+					lineSeries.Points.Add(new DataPoint(
+						DateTimeAxis.ToDouble(plotData.Key),
+						plotData.Value
+					));
+				}
+				lineSeries.YAxisKey = "Secondary";
+				lineSeries.Title = plotInfo.Key;
+				newSupplyGraphModel.Series.Add(lineSeries);
+			}
 
 			// グラフの要素を画面に反映する
 			newSupplyGraphModel.InvalidatePlot(true);
