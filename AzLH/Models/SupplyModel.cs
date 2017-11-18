b2179f12f944b28ba01c1109b4cb83bc1123204e
YSRKEN <ysr.ken@gmail.com>
2017-11-18 16:54:42 +0900

    資材データベースにデータが存在しない際にグラフが表示できない(例外が飛ぶ)ので改善した

--- AzLH/Models/SupplyModel.cs
+++ AzLH/Models/SupplyModel.cs
@@ -190,106 +190,108 @@ namespace AzLH.Models {
 		// グラフを再描画する
 		// 参考→https://qiita.com/maruh/items/035a39a2a01102ce248f
 		public void RedrawSupplyGraph() {
-			// Y軸および第二Y軸に表示するデータを集めておく
-			// Y軸→「表示する資材のモードと同じ」かつ「第二Y軸ではない」資材名
-			// 第二Y軸→「表示する資材のモードと同じ」かつ「第二Y軸である」資材名
-			var data = SupplyStore.GetSupplyData("燃料");
-			var AxisYList = CharacterRecognition.SupplyParameters
-				.Where(p => p.Value.MainSupplyFlg == (showSupplyMode == 0) && !p.Value.SecondaryAxisFlg)
-				.ToDictionary(p => p.Key, q => SupplyStore.GetSupplyData(q.Key));
-			var AxisY2List = CharacterRecognition.SupplyParameters
-				.Where(p => p.Value.MainSupplyFlg == (showSupplyMode == 0) && p.Value.SecondaryAxisFlg)
-				.ToDictionary(p => p.Key, q => SupplyStore.GetSupplyData(q.Key));
-			// グラフに必要な数値を算出する
-			//横軸
-			var maxYDateTime = AxisYList.Max(p => p.Value.Max(q => q.Key));
-			var maxY2DateTime = AxisY2List.Max(p => p.Value.Max(q => q.Key));
-			var maxDateTime = (maxYDateTime > maxY2DateTime ? maxYDateTime : maxY2DateTime);
-			var minDateTime = maxDateTime.AddDays(-NowGraphPeriodInfo.Days);
-			//縦軸
-			int maxYValue = AxisYList.Max(p => p.Value.Where(q => minDateTime <= q.Key).Max(r => r.Value));
-			int intervalY;
-			SpecialCeiling(ref maxYValue, out intervalY);
-			string axisYStr = "";
-			for (int i = 0; i < AxisYList.Count; ++i) {
-				if (i != 0)
-					axisYStr += "・";
-				axisYStr += AxisYList.Keys.ToList()[i];
-			}
-			int maxY2Value = AxisY2List.Max(p => p.Value.Where(q => minDateTime <= q.Key).Max(r => r.Value));
-			int intervalY2;
-			SpecialCeiling(ref maxY2Value, out intervalY2);
-			string axis2YStr = "";
-			for (int i = 0; i < AxisY2List.Count; ++i) {
-				if (i != 0)
-					axisYStr += "・";
-				axis2YStr += AxisY2List.Keys.ToList()[i];
-			}
-			// グラフ要素を構築する
-			//軸
-			var newSupplyGraphModel = new PlotModel();
-			newSupplyGraphModel.Axes.Add(new DateTimeAxis {
-				Position = AxisPosition.Bottom,
-				Minimum = DateTimeAxis.ToDouble(minDateTime),
-				Maximum = DateTimeAxis.ToDouble(maxDateTime),
-				StringFormat = NowGraphPeriodInfo.StringFormat,
-				MajorGridlineStyle = LineStyle.Solid,
-				MajorGridlineColor = OxyColors.LightGray,
-				MajorStep = NowGraphPeriodInfo.MajorStep,
-				MinorStep = NowGraphPeriodInfo.MinorStep,
-				Title = "日時"
-			});
-			newSupplyGraphModel.Axes.Add(new LinearAxis {
-				Position = AxisPosition.Left,
-				Minimum = 0.0,
-				Maximum = maxYValue,
-				MajorGridlineStyle = LineStyle.Solid,
-				MajorGridlineColor = OxyColors.LightGray,
-				MajorStep = intervalY,
-				MinorTickSize = 0,
-				Title = axisYStr,
-				Key = "Primary"
-			});
-			newSupplyGraphModel.Axes.Add(new LinearAxis {
-				Position = AxisPosition.Right,
-				Minimum = 0.0,
-				Maximum = maxY2Value,
-				MajorGridlineStyle = LineStyle.Solid,
-				MajorGridlineColor = OxyColors.LightGray,
-				MajorStep = intervalY2,
-				MinorTickSize = 0,
-				Title = axis2YStr,
-				Key = "Secondary"
-			});
-			//グラフ値
-			foreach(var plotInfo in AxisYList) {
-				var lineSeries = new LineSeries();
-				foreach(var plotData in plotInfo.Value) {
-					lineSeries.Points.Add(new DataPoint(
-						DateTimeAxis.ToDouble(plotData.Key),
-						plotData.Value
-					));
+			try {
+				// Y軸および第二Y軸に表示するデータを集めておく
+				// Y軸→「表示する資材のモードと同じ」かつ「第二Y軸ではない」資材名
+				// 第二Y軸→「表示する資材のモードと同じ」かつ「第二Y軸である」資材名
+				var AxisYList = CharacterRecognition.SupplyParameters
+					.Where(p => p.Value.MainSupplyFlg == (showSupplyMode == 0) && !p.Value.SecondaryAxisFlg)
+					.ToDictionary(p => p.Key, q => SupplyStore.GetSupplyData(q.Key));
+				var AxisY2List = CharacterRecognition.SupplyParameters
+					.Where(p => p.Value.MainSupplyFlg == (showSupplyMode == 0) && p.Value.SecondaryAxisFlg)
+					.ToDictionary(p => p.Key, q => SupplyStore.GetSupplyData(q.Key));
+				// グラフに必要な数値を算出する
+				//横軸
+				var maxYDateTime = AxisYList.Max(p => p.Value.Max(q => q.Key));
+				var maxY2DateTime = AxisY2List.Max(p => p.Value.Max(q => q.Key));
+				var maxDateTime = (maxYDateTime > maxY2DateTime ? maxYDateTime : maxY2DateTime);
+				var minDateTime = maxDateTime.AddDays(-NowGraphPeriodInfo.Days);
+				//縦軸
+				int maxYValue = AxisYList.Max(p => p.Value.Where(q => minDateTime <= q.Key).Max(r => r.Value));
+				int intervalY;
+				SpecialCeiling(ref maxYValue, out intervalY);
+				string axisYStr = "";
+				for (int i = 0; i < AxisYList.Count; ++i) {
+					if (i != 0)
+						axisYStr += "・";
+					axisYStr += AxisYList.Keys.ToList()[i];
 				}
-				lineSeries.YAxisKey = "Primary";
-				lineSeries.Title = plotInfo.Key;
-				newSupplyGraphModel.Series.Add(lineSeries);
-			}
-			foreach (var plotInfo in AxisY2List) {
-				var lineSeries = new LineSeries();
-				foreach (var plotData in plotInfo.Value) {
-					lineSeries.Points.Add(new DataPoint(
-						DateTimeAxis.ToDouble(plotData.Key),
-						plotData.Value
-					));
+				int maxY2Value = AxisY2List.Max(p => p.Value.Where(q => minDateTime <= q.Key).Max(r => r.Value));
+				int intervalY2;
+				SpecialCeiling(ref maxY2Value, out intervalY2);
+				string axis2YStr = "";
+				for (int i = 0; i < AxisY2List.Count; ++i) {
+					if (i != 0)
+						axisYStr += "・";
+					axis2YStr += AxisY2List.Keys.ToList()[i];
 				}
-				lineSeries.YAxisKey = "Secondary";
-				lineSeries.Title = plotInfo.Key;
-				newSupplyGraphModel.Series.Add(lineSeries);
-			}
+				// グラフ要素を構築する
+				//軸
+				var newSupplyGraphModel = new PlotModel();
+				newSupplyGraphModel.Axes.Add(new DateTimeAxis {
+					Position = AxisPosition.Bottom,
+					Minimum = DateTimeAxis.ToDouble(minDateTime),
+					Maximum = DateTimeAxis.ToDouble(maxDateTime),
+					StringFormat = NowGraphPeriodInfo.StringFormat,
+					MajorGridlineStyle = LineStyle.Solid,
+					MajorGridlineColor = OxyColors.LightGray,
+					MajorStep = NowGraphPeriodInfo.MajorStep,
+					MinorStep = NowGraphPeriodInfo.MinorStep,
+					Title = "日時"
+				});
+				newSupplyGraphModel.Axes.Add(new LinearAxis {
+					Position = AxisPosition.Left,
+					Minimum = 0.0,
+					Maximum = maxYValue,
+					MajorGridlineStyle = LineStyle.Solid,
+					MajorGridlineColor = OxyColors.LightGray,
+					MajorStep = intervalY,
+					MinorTickSize = 0,
+					Title = axisYStr,
+					Key = "Primary"
+				});
+				newSupplyGraphModel.Axes.Add(new LinearAxis {
+					Position = AxisPosition.Right,
+					Minimum = 0.0,
+					Maximum = maxY2Value,
+					MajorGridlineStyle = LineStyle.Solid,
+					MajorGridlineColor = OxyColors.LightGray,
+					MajorStep = intervalY2,
+					MinorTickSize = 0,
+					Title = axis2YStr,
+					Key = "Secondary"
+				});
+				//グラフ値
+				foreach (var plotInfo in AxisYList) {
+					var lineSeries = new LineSeries();
+					foreach (var plotData in plotInfo.Value) {
+						lineSeries.Points.Add(new DataPoint(
+							DateTimeAxis.ToDouble(plotData.Key),
+							plotData.Value
+						));
+					}
+					lineSeries.YAxisKey = "Primary";
+					lineSeries.Title = plotInfo.Key;
+					newSupplyGraphModel.Series.Add(lineSeries);
+				}
+				foreach (var plotInfo in AxisY2List) {
+					var lineSeries = new LineSeries();
+					foreach (var plotData in plotInfo.Value) {
+						lineSeries.Points.Add(new DataPoint(
+							DateTimeAxis.ToDouble(plotData.Key),
+							plotData.Value
+						));
+					}
+					lineSeries.YAxisKey = "Secondary";
+					lineSeries.Title = plotInfo.Key;
+					newSupplyGraphModel.Series.Add(lineSeries);
+				}
 
-			// グラフの要素を画面に反映する
-			newSupplyGraphModel.InvalidatePlot(true);
-			SupplyGraphModel = newSupplyGraphModel;
+				// グラフの要素を画面に反映する
+				newSupplyGraphModel.InvalidatePlot(true);
+				SupplyGraphModel = newSupplyGraphModel;
+			}
+			catch {}
 		}
 		// グラフ表示に適した適当な数値に切り上げる
 		private void SpecialCeiling(ref int maxValue, out int interval) {
