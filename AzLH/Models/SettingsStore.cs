612b6e410022e2730f85133327a400c62bb1588a
YSRKEN <ysr.ken@gmail.com>
2018-01-11 13:57:25 +0900

    static classはシングルトンパターンの夢を見るか？

--- AzLH/Models/SettingsStore.cs
+++ AzLH/Models/SettingsStore.cs
@@ -4,27 +4,29 @@ using System.IO;
 using System.Text;
 
 namespace AzLH.Models {
-	static class SettingStore {
-		
-	}
-	internal sealed class SettingsStore {
+	internal class SettingStore {
 		// シングルトンパターン
 		// 参考→https://qiita.com/rohinomiya/items/6bca22211d1bddf581c4
-		public static SettingsStore Instance { get; } = new SettingsStore();
-		private SettingsStore(){ SetDefaultSettings(); }
+		public static SettingStore This { get; } = new SettingStore();
+		private SettingStore(){}
+
+		#region ファイルに保存する設定
+		// メイン画面の位置・大きさ
+		public double[] MainWindowRect { get; set; }
+			= new double[] { double.NaN, double.NaN, 400.0, 300.0 };
+		// 資材記録画面の位置・大きさ
+		public double[] SupplyWindowRect { get; set; }
+			= new double[] { double.NaN, double.NaN, 600.0, 400.0 };
+		// 各種タイマー画面の位置・大きさ
+		public double[] TimerWindowRect { get; set; }
+			= new double[] { double.NaN, double.NaN, 400.0, 250.0 };
 
 		// Twitter用に加工するか？
 		public bool ForTwitterFlg { get; set; }
-		// メイン画面の位置・大きさ
-		public double[] MainWindowRect { get; set; }
-		// 資材記録画面の位置・大きさ
-		public double[] SupplyWindowRect { get; set; }
-		// 各種タイマー画面の位置・大きさ
-		public double[] TimerWindowRect { get; set; }
 		// ウィンドウの座標を記憶するか？
-		public bool MemoryWindowPositionFlg { get; set; }
+		public bool MemoryWindowPositionFlg { get; set; } = true;
 		// 常時座標を捕捉し続けるか？
-		public bool AutoSearchPositionFlg { get; set; }
+		public bool AutoSearchPositionFlg { get; set; } = true;
 		// 資材記録画面を最初から表示するか？
 		public bool AutoSupplyWindowFlg { get; set; }
 		// 各種タイマー画面を最初から表示するか？
@@ -36,12 +38,6 @@ namespace AzLH.Models {
 		// ドラッグ＆ドロップでシーン認識するか？
 		public bool DragAndDropPictureFlg { get; set; }
 
-		// 資材記録画面が表示されているか？
-		[JsonIgnore]
-		public bool ShowSupplyWindowFlg { get; set; }
-		// 各種タイマー画面が表示されているか？
-		[JsonIgnore]
-		public bool ShowTimerWindowFlg { get; set; }
 		// 軍事委託の完了時刻をメモする
 		public DateTime? ConsignFinalTime1 { get; set; }
 		public DateTime? ConsignFinalTime2 { get; set; }
@@ -52,6 +48,16 @@ namespace AzLH.Models {
 		public DateTime? LectureFinalTime2 { get; set; }
 		// 食糧の枯渇時刻をメモする
 		public DateTime? FoodFinalTime { get; set; }
+		#endregion
+
+		#region ファイルに保存しない設定
+		// 資材記録画面が表示されているか？
+		[JsonIgnore]
+		public bool ShowSupplyWindowFlg { get; set; }
+		// 各種タイマー画面が表示されているか？
+		[JsonIgnore]
+		public bool ShowTimerWindowFlg { get; set; }
+
 		// 各種ボムのチャージ完了時刻をメモする
 		[JsonIgnore]
 		public DateTime? BombChageTime1 { get; set; }
@@ -59,39 +65,20 @@ namespace AzLH.Models {
 		public DateTime? BombChageTime2 { get; set; }
 		[JsonIgnore]
 		public DateTime? BombChageTime3 { get; set; }
+		#endregion
 
-		// デフォルト設定
-		private void SetDefaultSettings() {
-			ForTwitterFlg = false;
-			MainWindowRect = new double[] { double.NaN, double.NaN, 400.0, 300.0 };
-			SupplyWindowRect = new double[] { double.NaN, double.NaN, 600.0, 400.0 };
-			TimerWindowRect = new double[] { double.NaN, double.NaN, 400.0, 250.0 };
-			MemoryWindowPositionFlg = true;
-			AutoSearchPositionFlg = true;
-			AutoSupplyWindowFlg = false;
-			AutoTimerWindowFlg = false;
-			AutoSupplyScreenShotFlg = false;
-			PutCharacterRecognitionFlg = false;
-			DragAndDropPictureFlg = false;
-			ConsignFinalTime1 = null;
-			ConsignFinalTime2 = null;
-			ConsignFinalTime3 = null;
-			ConsignFinalTime4 = null;
-			LectureFinalTime1 = null;
-			LectureFinalTime2 = null;
-			FoodFinalTime = null;
-			BombChageTime1 = null;
-			BombChageTime2 = null;
-			BombChageTime3 = null;
-		}
+		#region staticに処理するためのラッパー
+			
+		#endregion
+
 		// JSONから読み込み
-		public bool LoadSettings(string path) {
+		public static bool LoadSettings(string path = "settings.json") {
 			try {
 				using (var sr = new StreamReader(path, Encoding.UTF8)) {
 					// 全体をstringに読み込む
 					string json = sr.ReadToEnd();
 					// Json.NETでパース
-					var model = JsonConvert.DeserializeObject<SettingsStore>(json);
+					var model = JsonConvert.DeserializeObject<SettingStore>(json);
 					ForTwitterFlg = model.ForTwitterFlg;
 					MainWindowRect = model.MainWindowRect;
 					SupplyWindowRect = model.SupplyWindowRect;
@@ -112,43 +99,29 @@ namespace AzLH.Models {
 					FoodFinalTime = model.FoodFinalTime;
 				}
 				return true;
-			}
-			catch {
+			} catch {
 				return false;
 			}
 		}
-
-		public bool LoadSettings() {
-			return LoadSettings("settings.json");
-		}
-
 		// JSONに書き出し
-		public bool SaveSettings(string path) {
+		public static bool SaveSettings(string path = "settings.json") {
 			try {
 				using (var sw = new StreamWriter(path, false, Encoding.UTF8)) {
 					// Json.NETでstring形式に書き出す
-					string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+					string json = JsonConvert.SerializeObject(This, Formatting.Indented);
 					// 書き込み
 					sw.Write(json);
 				}
 				return true;
-			}
-			catch {
+			} catch {
 				return false;
 			}
 		}
-
-		public bool SaveSettings() {
-			return SaveSettings("settings.json");
-		}
-
 		// 設定項目を初期化
-		public bool Initialize() {
-			SetDefaultSettings();
+		public static bool Initialize() {
 			if (LoadSettings()) {
 				return true;
-			}
-			else {
+			} else {
 				SaveSettings();
 				return false;
 			}
