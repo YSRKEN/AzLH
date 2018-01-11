ce3d35faea16cdfa294ef2e91325eccf624928b2
YSRKEN <ysr.ken@gmail.com>
2018-01-12 03:15:17 +0900

    複数の座標候補があった場合に選択させる画面をリファクタリング

--- AzLH/ViewModels/GameScreenSelectViewModel.cs
+++ AzLH/ViewModels/GameScreenSelectViewModel.cs
@@ -3,49 +3,64 @@ using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Windows.Media.Imaging;
 using static AzLH.Models.MainModel;
 
 namespace AzLH.ViewModels {
-	internal class GameScreenSelectViewModel : IDisposable
+	internal class GameScreenSelectViewModel : IDisposable, INotifyPropertyChanged
 	{
-		// modelのinstance
-		private readonly GameScreenSelectModel gameScreenSelectModel;
 		private CompositeDisposable Disposable { get; } = new CompositeDisposable();
+		public event PropertyChangedEventHandler PropertyChanged;
+		// rect一覧
+		private readonly List<Rectangle> rectList;
+		// 選択しているrectのindex
+		private ReactiveProperty<int> RectIndex { get; } = new ReactiveProperty<int>(0);
+		// 選択結果を返すdelegate
+		private readonly SelectGameWindowAction dg;
+
 		// trueにすると画面を閉じる
-		public ReactiveProperty<bool> CloseWindow { get; }
+		public ReactiveProperty<bool> CloseWindow { get; } = new ReactiveProperty<bool>();
 		// ページ情報を表示
 		public ReactiveProperty<string> PageInfoStr { get; }
 		// 選択しているrectに基づくスクショのプレビュー
 		public ReactiveProperty<BitmapSource> GameWindowPage { get; }
+
 		// 前の画像に移動
-		public ReactiveCommand PrevPageCommand { get; }
+		public ReactiveCommand PrevPageCommand { get; } = new ReactiveCommand();
 		// 次の画像に移動
-		public ReactiveCommand NextPageCommand { get; }
+		public ReactiveCommand NextPageCommand { get; } = new ReactiveCommand();
 		// 決定ボタン
-		public ReactiveCommand SelectPageCommand { get; }
+		public ReactiveCommand SelectPageCommand { get; } = new ReactiveCommand();
 		// キャンセルボタン
-		public ReactiveCommand CancelCommand { get; }
+		public ReactiveCommand CancelCommand { get; } = new ReactiveCommand();
+
 		// コンストラクタ
 		public GameScreenSelectViewModel() { }
 		public GameScreenSelectViewModel(List<Rectangle> rectList, SelectGameWindowAction dg) {
-			// 初期化
-			gameScreenSelectModel = new GameScreenSelectModel(rectList, dg);
+			this.rectList = rectList;
+			this.dg = dg;
 			// プロパティを設定
-			CloseWindow = gameScreenSelectModel.ObserveProperty(x => x.CloseWindow).ToReactiveProperty().AddTo(Disposable);
-			PageInfoStr = gameScreenSelectModel.ObserveProperty(x => x.PageInfoStr).ToReactiveProperty().AddTo(Disposable);
-			GameWindowPage = gameScreenSelectModel.ObserveProperty(x => x.GameWindowPage).ToReactiveProperty().AddTo(Disposable);
+			PageInfoStr = RectIndex.Select(x => $"[{x + 1}/{rectList.Count}] {Utility.GetRectStr(rectList[x])}").ToReactiveProperty();
+			GameWindowPage = RectIndex.Select(x => (BitmapSource)ScreenShotProvider.GetScreenBitmap(rectList[x]).ToImageSource()).ToReactiveProperty();
 			// コマンドを設定
-			PrevPageCommand = new ReactiveCommand();
-			NextPageCommand = new ReactiveCommand();
-			SelectPageCommand = new ReactiveCommand();
-			CancelCommand = new ReactiveCommand();
-			PrevPageCommand.Subscribe(gameScreenSelectModel.PrevPage);
-			NextPageCommand.Subscribe(gameScreenSelectModel.NextPage);
-			SelectPageCommand.Subscribe(gameScreenSelectModel.SelectPage);
-			CancelCommand.Subscribe(gameScreenSelectModel.Cancel);
+			PrevPageCommand.Subscribe(_ => {
+				RectIndex.Value = (rectList.Count + RectIndex.Value - 1) % rectList.Count;
+			});
+			NextPageCommand.Subscribe(_ => {
+				RectIndex.Value = (rectList.Count + RectIndex.Value + 1) % rectList.Count;
+			});
+			SelectPageCommand.Subscribe(_ => {
+				dg(rectList[RectIndex.Value]);
+				CloseWindow.Value = true;
+			});
+			CancelCommand.Subscribe(_ => {
+				dg(null);
+				CloseWindow.Value = true;
+			});
 		}
 
 		// Dispose処理
