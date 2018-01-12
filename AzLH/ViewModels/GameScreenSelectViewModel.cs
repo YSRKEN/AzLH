cdbfcb4e2c7ac0c7c54b774e681d83c36f138cee
YSRKEN <ysr.ken@gmail.com>
2018-01-12 13:47:39 +0900

    ReadOnlyなReactivePropertyをReadOnlyReactivePropertyに変更

--- AzLH/ViewModels/GameScreenSelectViewModel.cs
+++ AzLH/ViewModels/GameScreenSelectViewModel.cs
@@ -25,9 +25,9 @@ namespace AzLH.ViewModels {
 		// trueにすると画面を閉じる
 		public ReactiveProperty<bool> CloseWindow { get; } = new ReactiveProperty<bool>();
 		// ページ情報を表示
-		public ReactiveProperty<string> PageInfoStr { get; }
+		public ReadOnlyReactiveProperty<string> PageInfoStr { get; }
 		// 選択しているrectに基づくスクショのプレビュー
-		public ReactiveProperty<BitmapSource> GameWindowPage { get; }
+		public ReadOnlyReactiveProperty<BitmapSource> GameWindowPage { get; }
 
 		// 前の画像に移動
 		public ReactiveCommand PrevPageCommand { get; } = new ReactiveCommand();
@@ -44,8 +44,8 @@ namespace AzLH.ViewModels {
 			this.rectList = rectList;
 			this.dg = dg;
 			// プロパティを設定
-			PageInfoStr = RectIndex.Select(x => $"[{x + 1}/{rectList.Count}] {Utility.GetRectStr(rectList[x])}").ToReactiveProperty();
-			GameWindowPage = RectIndex.Select(x => (BitmapSource)ScreenShotProvider.GetScreenBitmap(rectList[x]).ToImageSource()).ToReactiveProperty();
+			PageInfoStr = RectIndex.Select(x => $"[{x + 1}/{rectList.Count}] {Utility.GetRectStr(rectList[x])}").ToReadOnlyReactiveProperty();
+			GameWindowPage = RectIndex.Select(x => (BitmapSource)ScreenShotProvider.GetScreenBitmap(rectList[x]).ToImageSource()).ToReadOnlyReactiveProperty();
 			// コマンドを設定
 			PrevPageCommand.Subscribe(_ => {
 				RectIndex.Value = (rectList.Count + RectIndex.Value - 1) % rectList.Count;
